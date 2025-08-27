e5ac1729b740bc2a7a09d3947f091ea9bd8b5b5c
Romario <romarioinfobraz@gmail.com>
2025-08-27 16:42:07 -0300

    Conclusão do desafio.
    Todos as solicitações "To do" atendidas.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConexaoPostgres1")));
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
